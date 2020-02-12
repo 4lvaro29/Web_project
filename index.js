33fc8ff7966dff8fc1bd6aef48f135743c4b9b8d
alvaro <4lvaro.up@gmail.com>
2020-02-11 22:51:35 -0300

    FIX: Procfile

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-express = require("express"),
-app = express(),
-port = process.env.PORT || 3000;
+// var express = require("express"),
+//  app = express(),
+//  port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/public/'));
+// app.use(express.static(__dirname + '/public/'));
 
-    app.listen(port,function(){
-      console.log(`server ok listen in port #{port}`)
+//     app.listen(port,function(){
+//       console.log(`server ok listen in port #{port}`)
     
-    });
+//     });
