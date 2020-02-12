b08e379a25a0e0c110d571ad26d0fdf6c5928c7a
alvaro <4lvaro.up@gmail.com>
2020-02-11 22:19:35 -0300

    ADD:package.json

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var express = require("express"),
-var path = require("path"),
+path = require("path"),
 var app = express(),
 var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public/'));
 
-    app.listen('3000',function(){
+    app.listen(port,function(){
       console.log(`server ok listen in port #{port}`)
     
     });
