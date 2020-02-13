12dd96637e4466c6991f117356c383fed934f5bc
alvaro <4lvaro.up@gmail.com>
2020-02-13 13:21:42 -0300

    package updated2

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const express = require('express')
-const app = express();
+var express = require('express');
+var bodyParser = require('body-parser');
 
-app.get('/', (req, res) => {
-  res.send('Alvaro')
+var app = express();
+var port = process.env.PORT || 3525;
+
+// Convierte una petición recibida (POST-GET...) a objeto JSON
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res){
+	res.status(200).send({
+		message: 'GET Home route working fine!'
+	});
 });
 
-app.listen(8000, () => {
-  console.log('Example app listening on port 8000!')
+app.listen(port, function(){
+	console.log(`Server running in http://localhost:${port}`);
+	console.log('Defined routes:');
+	console.log('	[GET] http://localhost:3525/');
 });
